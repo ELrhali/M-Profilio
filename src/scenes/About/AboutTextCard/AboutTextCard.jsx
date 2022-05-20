b4374f1a38cfe6a44b1e3e3da657aa65ca3ee5a0
ELrhali <mohammedelrhali98@gmail.com>
2022-05-20 21:52:36 +0100

    FIRST

---
+++ src/scenes/About/AboutTextCard/AboutTextCard.jsx
@@ -0,0 +1,43 @@
+import s from './AboutTextCard.module.scss';
+import { ImPointRight } from 'react-icons/im';
+
+const AboutTextCard = () => {
+  return (
+    <div className={s.card}>
+      <p style={{ textAlign: 'justify' }}>
+        Hello Everyone, I am{' '}
+        <span className={s.purple}>El Rhali Mohammed </span>
+        from <span className={s.purple}> Tangier, Morroco.</span>
+        <br />
+        I'm a Software Developer & Content Creator, currently working as JavaScript React Web Developer.
+        <br />
+        <br />
+        Apart from coding, some other activities that I love to do!
+      </p>
+
+      <ul>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Playing Games
+        </li>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Reading
+        </li>
+        <li className={s.aboutActivity}>
+          <ImPointRight /> Travelling
+        </li>
+      </ul>
+
+      <p
+        style={{
+          color: 'rgb(155 126 172)',
+          textAlign: 'center',
+          marginTop: '1rem',
+        }}
+      >
+        "Strive to build things that make a difference!"{' '}
+      </p>
+    </div>
+  );
+};
+
+export default AboutTextCard;
